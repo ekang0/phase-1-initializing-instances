2ceb351b72237d53a2ea13753caf4c275c4b0ac0
Esther Kang <eskang87@gmail.com>
2022-01-24 11:46:43 -0800

    commit tree fea1e23ed6728374c67714b64140a76a463b232e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+
+class Breakfast {
+  constructor(food, drink){
+    this.food = food,
+    this.drink = drink
+  }
+};
+
+class Lunch {
+  constructor(salad, soup, drink){
+    this.salad = salad,
+    this.soup = soup,
+    this.drink = drink
+  }
+};
+
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad,
+    this.soup = soup,
+    this.entree = entree,
+    this.#dessert = dessert
+  }
+};
+
+//salad, soup, entree, and dessert
+//Initialize dessert as a private property by prefixing its name with the hash symbol (#).
